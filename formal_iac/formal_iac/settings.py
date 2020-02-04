99e056b59cc6642adbfd91bb75c0c25b97152a3c
Gonzalo Rubio <gonzalo.rubio.casas@gmail.com>
2020-02-04 16:02:18 +0100

    Created first example view inside playbooks_parser app

--- formal_iac/formal_iac/settings.py
+++ formal_iac/formal_iac/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'playbooks_parser.apps.PlaybooksParserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- formal_iac/playbooks_parser/views.py
+++ formal_iac/playbooks_parser/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
-# Create your views here.
+# Create your views here
+
+def index(request):
+    template = loader.get_template("playbooks_parser/index.html")
+    return HttpResponse(template.render())
