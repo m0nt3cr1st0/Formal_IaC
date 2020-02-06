b6661f79f5ab109b1072fe80657facac5eb95603
Gonzalo Rubio <gonzalo.rubio.casas@gmail.com>
2020-02-06 17:03:50 +0100

    Added models for Package and State

--- formal_iac/playbooks_parser/models.py
+++ formal_iac/playbooks_parser/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
+class Package(models.Model):
+    package_name = models.CharField(max_length=200)
+    package_version = models.FloatField
+
+
 # Create your models here.
+class State(models.Model):
+    set_of_packages = models.ManyToManyField(Package)
+
+
+
