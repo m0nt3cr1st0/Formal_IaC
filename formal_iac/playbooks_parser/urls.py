b968dcde857436c1589714c6c9de03fbc0d88ecf
Gonzalo Rubio <gonzalo.rubio.casas@gmail.com>
2020-02-20 19:09:00 +0100

    Added views for a playbook and a parsed playbook

--- formal_iac/playbooks_parser/urls.py
+++ formal_iac/playbooks_parser/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # ex: /playbooks_parser/5
+    path('<int:playbook_id>', views.playbook, name='playbook'),
+    # ex: /playbooks_parser/5/parsed/
+    path('<int:playbook_id>/parsed/', views.playbook_parsed, name='playbook_parsed')
 ]
--- formal_iac/playbooks_parser/views.py
+++ formal_iac/playbooks_parser/views.py
@@ -1,9 +1,24 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.template import loader
 
-# Create your views here
+from .models import Task, Playbook
+
+import yaml
+
 
 def index(request):
     template = loader.get_template("playbooks_parser/index.html")
     return HttpResponse(template.render())
+
+
+def playbook(request, playbook_id):
+    first_playbook = Playbook.objects.order_by('name')[0]
+    output = first_playbook.playbook_name
+    return HttpResponse(output)
+
+
+def playbook_parsed(request, playbook_id):
+    first_playbook = Playbook.objects.order_by('name')[0]
+    output = first_playbook.playbook_name
+    return HttpResponse("You're looking at playbook %s parsed" % playbook_id)
+
