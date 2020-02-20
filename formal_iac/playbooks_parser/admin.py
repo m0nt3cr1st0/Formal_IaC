73f7c057e3ed533067d2b66ca29180dfba3ee28b
Gonzalo Rubio <gonzalo.rubio.casas@gmail.com>
2020-02-20 18:55:56 +0100

    Added Task and Playbook model

--- formal_iac/playbooks_parser/admin.py
+++ formal_iac/playbooks_parser/admin.py
@@ -1,3 +1,9 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import State, Package, Task, Playbook
+
+admin.site.register(Package)
+admin.site.register(State)
+admin.site.register(Task)
+admin.site.register(Playbook)
--- formal_iac/playbooks_parser/models.py
+++ formal_iac/playbooks_parser/models.py
@@ -5,10 +5,23 @@ class Package(models.Model):
     package_name = models.CharField(max_length=200)
     package_version = models.FloatField
 
+    def __str__(self):
+        return self.package_name
 
-# Create your models here.
+
 class State(models.Model):
     set_of_packages = models.ManyToManyField(Package)
 
 
+class Task(models.model):
+    task_name = models.CharField(max_length=200)
+    task_module = models.CharField(max_length=200)
+    # module_arguments should be a list of strings for each option
+    module_options = models.CharField(max_length=200)
+    # module_arguments should be a list of strings for each argument
+    module_arguments = models.CharField(max_length=200)
+
+
+class Playbook(models.model):
+    list_of_tasks = models.ManyToManyField(Task)
 
