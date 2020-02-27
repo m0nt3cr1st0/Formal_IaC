b6677e595a77c5577ef1b534883bda6ed15dfe89
Gonzalo Rubio <gonzalo.rubio.casas@gmail.com>
2020-02-27 17:41:24 +0100

    Added urls to display a  playbook, a parsed playbook and a state

--- formal_iac/playbooks_parser/urls.py
+++ formal_iac/playbooks_parser/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = 'playbooks_parser'
 urlpatterns = [
+    # ex: /playbooks_parser/
     path('', views.index, name='index'),
     # ex: /playbooks_parser/5
     path('<int:playbook_id>', views.playbook, name='playbook'),
     # ex: /playbooks_parser/5/parsed/
-    path('<int:playbook_id>/parsed/', views.playbook_parsed, name='playbook_parsed')
+    path('<int:playbook_id>/parsed/', views.playbook_parsed, name='playbook_parsed'),
+    # ex: /playbooks_parser/states
+    path('states', views.state, name='state')
 ]
