2ec66831f4f524274f1f633054eca882f8b22c8d
Gonzalo Rubio <gonzalo.rubio.casas@gmail.com>
2020-02-27 17:39:06 +0100

    Updated views for index and playbook

--- formal_iac/playbooks_parser/views.py
+++ formal_iac/playbooks_parser/views.py
@@ -1,24 +1,28 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import get_object_or_404, render
 
-from .models import Task, Playbook
+from .models import Task, Playbook, State
 
 import yaml
 
 
+# First way of returning a rendered template
 def index(request):
+    latest_uploaded_playbooks = Playbook.objects.order_by('id')[:5]
     template = loader.get_template("playbooks_parser/index.html")
-    return HttpResponse(template.render())
+    context = {
+        latest_uploaded_playbooks: latest_uploaded_playbooks,
+    }
+    return HttpResponse(template.render(context, request))
 
 
-def playbook(request):
-    first_playbook = Playbook.objects.order_by('playbook_name')[0]
-    output = first_playbook.playbook_name
-    return HttpResponse(output)
+def playbook(request, playbook_id):
+    requested_playbook = get_object_or_404(Playbook, pk=playbook_id)
+    context = {
+        'requested_ṕlaybook': requested_playbook
+    }
+    return render(request, 'playbooks/playbook.html', context)
 
 
-def playbook_parsed(request, playbook_id):
-    first_playbook = Playbook.objects.order_by('playbook_name')[0]
-    output = first_playbook.playbook_name
-    return HttpResponse("You're looking at playbook %s parsed" % playbook_id)
 
