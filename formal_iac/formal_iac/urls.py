5fe8ed13aee4f517a871f553214b83f9108b3506
Gonzalo Rubio <gonzalo.rubio.casas@gmail.com>
2020-03-20 13:32:08 +0100

    Fixed same name url warning on project URLconf

--- formal_iac/formal_iac/urls.py
+++ formal_iac/formal_iac/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
-    path('', admin.site.urls),
     path('admin/', admin.site.urls),
     path('playbooks_parser/', include('playbooks_parser.urls')),
 ]
