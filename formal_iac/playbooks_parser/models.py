320a5ac0197c7283ad3996d4bf0e45f8b74cf579
Gonzalo Rubio <gonzalo.rubio.casas@gmail.com>
2020-03-20 13:33:06 +0100

    Add _str_ method to all models

--- formal_iac/playbooks_parser/models.py
+++ formal_iac/playbooks_parser/models.py
@@ -3,15 +3,18 @@ from django.db import models
 
 class Package(models.Model):
     package_name = models.CharField(max_length=200)
-    package_version = models.FloatField
+    package_version = models.FloatField()
 
     def __str__(self):
-        return self.package_name
+        return self.package_name + " " + str(self.package_version)
 
 
 class State(models.Model):
     state_name = models.CharField(max_length=200)
     set_of_packages = models.ManyToManyField(Package)
+
+    def __str__(self):
+        return self.state_name
 
 
 class Task(models.Model):
@@ -22,9 +25,14 @@ class Task(models.Model):
     # module_arguments should be a list of strings for each argument
     module_arguments = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.task_name
+
 
 class Playbook(models.Model):
     playbook_name = models.CharField(max_length=200)
     playbook_content = models.TextField()
     list_of_tasks = models.ManyToManyField(Task)
 
+    def __str__(self):
+        return self.playbook_name
