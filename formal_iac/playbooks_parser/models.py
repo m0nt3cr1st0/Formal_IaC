afafe41cd9c731412870c9e3cd7a13dad07dbc62
Gonzalo Rubio <gonzalo.rubio.casas@gmail.com>
2020-05-30 11:24:59 +0200

    Remove additional version field to simplify the analysis
    Despite it seem a obviously necessary field, in order to simplify
    our analysis, based on the information the security tracker provides
    where no package version it is specified on the general view
    We will get rid of that in our analysis too.

--- formal_iac/playbooks_parser/models.py
+++ formal_iac/playbooks_parser/models.py
@@ -9,11 +9,10 @@ class Vulnerability(models.Model):
 
 class Package(models.Model):
     package_name = models.CharField(max_length=200)
-    package_version = models.FloatField()
     set_of_vulnerabilities = models.ManyToManyField(Vulnerability)
 
     def __str__(self):
-        return self.package_name + " " + str(self.package_version)
+        return self.package_name
 
 
 class State(models.Model):
