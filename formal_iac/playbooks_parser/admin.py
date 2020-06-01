a527564ba134e3b0140bb275953c6292a3915970
Gonzalo Rubio <gonzalo.rubio.casas@gmail.com>
2020-06-01 16:58:19 +0200

    Include Vulnerability model in the admin interface

--- formal_iac/playbooks_parser/admin.py
+++ formal_iac/playbooks_parser/admin.py
@@ -1,10 +1,11 @@
 
 from django.contrib import admin
 
-from .models import State, Package, Task, Playbook, PlaybookExecution
+from .models import Package, Playbook, PlaybookExecution, State, Task, Vulnerability
 
 admin.site.register(Package)
-admin.site.register(State)
-admin.site.register(Task)
 admin.site.register(Playbook)
 admin.site.register(PlaybookExecution)
+admin.site.register(State)
+admin.site.register(Task)
+admin.site.register(Vulnerability)
