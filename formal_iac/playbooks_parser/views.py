ea72ab2cb2d010e60a1e933d18c3e6d082423389
Gonzalo Rubio <gonzalo.rubio.casas@gmail.com>
2020-06-14 12:12:39 +0200

    Integrate fsm into the demo result and remove legacy code

--- formal_iac/playbooks_parser/views.py
+++ formal_iac/playbooks_parser/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import get_object_or_404, render, redirect
 
-from .auxiliary_functions import create_playbook, create_playbook_execution
+from .auxiliary_functions import build_fsm_from_execution, create_playbook, create_playbook_execution
 from .forms import ParsePlaybookDemoForm, SelectAnalysisForm
 from .models import Playbook
 
@@ -69,33 +69,16 @@ def demo_result_view(request):
             # With the playbook created/retrieved access its content and analyze the packages to be installed
             if playbook_to_analyze != "":
                 playbook_execution = create_playbook_execution(playbook_to_analyze)
-                list_of_tasks = playbook_to_analyze.list_of_tasks
+                fsm_representing_execution = build_fsm_from_execution(playbook_execution)
+                # list_of_tasks = playbook_to_analyze.list_of_tasks
             else:
-                list_of_tasks = []
+                # list_of_tasks = []
+                fsm_representing_execution = []
             context = {
-                'playbook_tasks': list_of_tasks,
+                # 'playbook_tasks': list_of_tasks,
+                'playbook_tasks': fsm_representing_execution,
                 'playbook_execution': playbook_execution
             }
             return render(request, "playbooks_parser/demo_result.html", context)
     return {}
-
-
-# Quick PoC
-def graph_view(request):
-    dot = Digraph(comment='plotted state')
-    dot.node('A', '[yum_a, ansible_b]')
-    dot.node('B', '[test_1, test_2]')
-    dot.edge('A', 'B', 'add test_1')
-    dot.format = 'svg'
-    context = {
-        'rendered_state': dot.pipe().decode('utf-8')
-    }
-    # Algorithm:
-    # You request the run of a playbook on /playbooks_parser/<playbook_id>/run
-    # Get list of vulnerable packages
-    # Get initial state and add it as a node
-    # For every action define a transition and register that state
-    # If you are adding/updating a package check the package with the list
-    # Color it if its on it with red or orange, or green if not
-    return render(request, 'playbooks_parser/graph.html', context)
 
